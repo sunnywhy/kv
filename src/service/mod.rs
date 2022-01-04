7ebda26753b09e44451063bd266f939bd00f025c
sunnywhy <sunnywhy@gmail.com>
2022-01-04 00:20:41 -0500

    add listener events

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -28,14 +28,13 @@ impl<Store> Clone for Service<Store> {
 
 pub struct ServiceInner<Store> {
     store: Store,
+    on_received: Vec<fn(&CommandRequest)>,
+    on_executed: Vec<fn(&CommandResponse)>,
+    on_before_send: Vec<fn(&mut CommandResponse)>,
+    on_after_send: Vec<fn()>,
 }
 
 impl<Store: Storage> Service<Store> {
-    pub fn new(store: Store) -> Self {
-        Self {
-            inner: Arc::new(ServiceInner { store }),
-        }
-    }
 
     pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
         debug!("Got request: {:?}", cmd);
@@ -44,6 +43,46 @@ impl<Store: Storage> Service<Store> {
         // TODO: emit on_executed event
 
         res
+    }
+}
+
+impl<Store: Storage> ServiceInner<Store> {
+    pub fn new(store: Store) -> Self {
+        Self { 
+            store, 
+            on_received: Vec::new(),
+            on_executed: Vec::new(),
+            on_before_send: Vec::new(),
+            on_after_send: Vec::new(),
+        }
+    }
+
+    pub fn fn_received(mut self, f: fn(&CommandRequest)) -> Self {
+        self.on_received.push(f);
+        self
+    }
+
+    pub fn fn_executed(mut self, f: fn(&CommandResponse)) -> Self {
+        self.on_executed.push(f);
+        self
+    }
+
+    pub fn fn_before_send(mut self, f: fn(&mut CommandResponse)) -> Self {
+        self.on_before_send.push(f);
+        self
+    }
+
+    pub fn fn_after_send(mut self, f: fn()) -> Self {
+        self.on_after_send.push(f);
+        self
+    }
+}
+
+impl<Store: Storage> From<ServiceInner<Store>> for Service<Store> {
+    fn from(inner: ServiceInner<Store>) -> Self {
+        Self {
+            inner: Arc::new(inner),
+        }
     }
 }
 
@@ -66,6 +105,9 @@ fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
 mod tests {
     use std::thread;
 
+    use http::StatusCode;
+    use tracing::info;
+
     use crate::Value;
 
     use super::*;
@@ -73,7 +115,7 @@ mod tests {
     #[test]
     fn service_should_work() {
         // we need a service to include the Storage
-        let service = Service::new(MemTable::default());
+        let service: Service = ServiceInner::new(MemTable::default()).into();
 
         // service should able to run in multiple threads
         let cloned = service.clone();
@@ -90,6 +132,38 @@ mod tests {
         let res = service.execute(CommandRequest::new_hget("t1", "k1"));
         assert_res_ok(res, &["v1".into()], &[]);
     }
+
+    #[test]
+    fn event_registration_should_work() {
+        fn b(cmd: &CommandRequest) {
+            info!("Got {:?}", cmd);
+        }
+
+        fn c(res: &CommandResponse) {
+            info!("{:?}", res);
+        }
+
+        fn d(res: &mut CommandResponse) {
+            res.status = StatusCode::CREATED.as_u16() as _;
+        }
+
+        fn e() {
+            info!("Data is sent");
+        }
+
+        let service: Service = ServiceInner::new(MemTable::default())
+            .fn_received(|_: &CommandRequest| {})
+            .fn_received(b)
+            .fn_executed(c)
+            .fn_before_send(d)
+            .fn_after_send(e)
+            .into();
+        
+            let res = service.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            assert_eq!(res.status, StatusCode::CREATED.as_u16() as _);
+            assert_eq!(res.message, "");
+            assert_eq!(res.values, vec![Value::default()]);
+     }
 }
 
 #[cfg(test)]
