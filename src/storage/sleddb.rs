fc28d8c74c6006719896b363539c0c96b9d1bd9a
sunnywhy <sunnywhy@gmail.com>
2022-01-07 23:36:37 -0500

    sleddb table support

--- src/storage/sleddb.rs
+++ src/storage/sleddb.rs
@@ -9,4 +9,14 @@ impl SledDb {
     pub fn new(path: impl AsRef<Path>) -> Self {
         Self(sled::open(path).unwrap())
     }
+
+    // in sleddb, since it can do scan_prefix, we use prefix to simulate a table
+    fn get_full_key(table: &str, key: &str) -> String {
+        format!("{}:{}", table, key)
+    }
+
+    // when iterating through the table keys, we use prefix: as table
+    fn get_table_prefix(table: &str) -> String {
+        format!("{}:", table)
+    }
 }
