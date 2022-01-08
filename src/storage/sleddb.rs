e6bb08ab9a38dcb6ca1338b94f59215592b6f457
sunnywhy <sunnywhy@gmail.com>
2022-01-07 23:47:53 -0500

    sleddb default storage

--- src/storage/sleddb.rs
+++ src/storage/sleddb.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 
 use sled::Db;
 
+use crate::Storage;
+
 #[derive(Debug)]
 pub struct SledDb(Db);
 
@@ -18,5 +20,36 @@ impl SledDb {
     // when iterating through the table keys, we use prefix: as table
     fn get_table_prefix(table: &str) -> String {
         format!("{}:", table)
+    }
+}
+
+/// flip Option<Result<T, E>> into Result<Option<T>, E>
+fn flip<T, E>(x: Option<Result<T, E>>) -> Result<Option<T>, E> {
+    x.map_or(Ok(None), |v| v.map(Some))
+}
+
+impl Storage for SledDb {
+    fn get(&self, table: &str, key: &str) -> Result<Option<crate::Value>, crate::KvError> {
+        todo!()
+    }
+
+    fn set(&self, table: &str, key: String, value: crate::Value) -> Result<Option<crate::Value>, crate::KvError> {
+        todo!()
+    }
+
+    fn contains(&self, table: &str, key: &str) -> Result<bool, crate::KvError> {
+        todo!()
+    }
+
+    fn del(&self, table: &str, key: &str) -> Result<Option<crate::Value>, crate::KvError> {
+        todo!()
+    }
+
+    fn get_all(&self, table: &str) -> Result<Vec<crate::Kvpair>, crate::KvError> {
+        todo!()
+    }
+
+    fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = crate::Kvpair>>, crate::KvError> {
+        todo!()
     }
 }
