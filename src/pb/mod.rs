8cda2ec8eb498e251e4d3aeccf63b3c347474ad9
sunnywhy <sunnywhy@gmail.com>
2022-01-08 18:11:02 -0500

    try from

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -2,6 +2,7 @@ pub mod abi;
 
 use abi::{command_request::RequestData, *};
 use http::StatusCode;
+use prost::Message;
 
 use crate::KvError;
 
@@ -190,5 +191,23 @@ impl From<Vec<Value>> for CommandResponse {
             values: vec,
             ..Default::default()
         }
+    }
+}
+
+impl TryFrom<Value> for Vec<u8> {
+    type Error = KvError;
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        let mut buf = Vec::with_capacity(v.encoded_len());
+        v.encode(&mut buf)?;
+        Ok(buf)
+    }
+}
+
+impl TryFrom<&[u8]> for Value {
+    type Error = KvError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let msg = Value::decode(value)?;
+        Ok(msg)
     }
 }
